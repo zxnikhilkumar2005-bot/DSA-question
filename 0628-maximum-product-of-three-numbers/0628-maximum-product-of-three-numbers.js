5adbf287a35b038c3592f2793a5a6a0444b391fe
NIkhil kumar <zxnikhilkumar2005@gmail.com>
2026-07-26 12:06:23 +0530

    Time: 41 ms (6.7%), Space: 58.4 MB (79.91%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    const n = nums.length;
+
+    return Math.max(
+        nums[n - 1] * nums[n - 2] * nums[n - 3],
+        nums[0] * nums[1] * nums[n - 1]
+    );
+};
