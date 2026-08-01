ccc193ee2f71738ee41f6b458385f8c2450d8253
NIkhil kumar <zxnikhilkumar2005@gmail.com>
2026-08-01 09:43:20 +0530

    Time: 2 ms (43.58%), Space: 10.2 MB (32.54%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
+    int solve(int i, int j, vector<int>& nums, vector<vector<int>>& dp) {
+        if (i == j) return nums[i];
+
+        if (dp[i][j] != INT_MIN)
+            return dp[i][j];
+
+        int left = nums[i] - solve(i + 1, j, nums, dp);
+        int right = nums[j] - solve(i, j - 1, nums, dp);
+
+        return dp[i][j] = max(left, right);
+    }
+
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
 
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-
-        // Base case
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = nums[i];
-        }
-
-        // Fill DP table
-        for (int len = 2; len <= n; len++) {
-            for (int i = 0; i + len - 1 < n; i++) {
-                int j = i + len - 1;
-
-                int takeLeft = nums[i] - dp[i + 1][j];
-                int takeRight = nums[j] - dp[i][j - 1];
-
-                dp[i][j] = max(takeLeft, takeRight);
-            }
-        }
-
-        return dp[0][n - 1] >= 0;
+        return solve(0, n - 1, nums, dp) >= 0;
     }
 };
